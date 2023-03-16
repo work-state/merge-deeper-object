eeab31f4128d06ee42fd72d3e1499f943e1a07d9
work state <work.state.09@gmail.com>
2023-03-16 19:46:10 +0100

    [fix] adjust array condition

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 module.exports = function mergeDeeper(object, merge) {
   if (!merge || typeof merge === "string") merge = {};
   [...Object.keys(merge), ...Object.getOwnPropertySymbols(merge)].map((el) => {
-    if (
-      typeof merge[el] !== typeof object[el] ||
-      (typeof merge[el] && typeof object[el]) !== "object" ||
-      !merge[el]
-    )
-      object[el] = merge[el];
-    else if (Array.isArray(object[el]) && Array.isArray(merge[el]))
-      merge[el] = [...object[el], ...merge[el]];
-
-    if (!(el in object))
-      object[el] =
-        typeof merge[el] === "object" && !Array.isArray(merge[el])
-          ? { ...merge[el] }
-          : merge[el];
-
-    mergeDeeper(object[el], merge[el]);
+    if (el in object && Array.isArray(merge[el]) && Array.isArray(object[el])) {
+      object[el] = [...object[el], ...merge[el]];
+      mergeDeeper([], []);
+    } else {
+      if (
+        typeof merge[el] !== typeof object[el] ||
+        (typeof merge[el] && typeof object[el]) !== "object" ||
+        !merge[el]
+      )
+        object[el] = merge[el];
+      if (!(el in object))
+        object[el] =
+          typeof merge[el] === "object" && !Array.isArray(merge[el])
+            ? { ...merge[el] }
+            : merge[el];
+      mergeDeeper(object[el], merge[el]);
+    }
   });
   return object;
 };
